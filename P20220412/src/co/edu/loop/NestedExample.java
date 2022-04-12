33d7e0dbcb3e40cba2ce736e35dcc76d151f26d9
admin <admin@YD2-06>
2022-04-12 17:52:17 +0900

    111

--- P20220412/src/co/edu/loop/NestedExample.java
+++ P20220412/src/co/edu/loop/NestedExample.java
@@ -22,8 +22,8 @@ public class NestedExample {
 		System.out.println();
 
          // p161  6번
-		for(int i = 20; i >= 1; i--) {
-			for(int j = 1; j <= 20; j++) {
+		for(int i = 10; i >= 1; i--) {
+			for(int j = 1; j <= 10; j++) {
 				if(j < i) {
 					System.out.print(" ");	
 				} else {
@@ -35,27 +35,27 @@ public class NestedExample {
 		
 		
 		// p161 3번
-		int dice1;
-		int dice2;
-		boolean isTrue = true;
-		while(isTrue) {
-			dice1 = (int)(Math.random() * 6) + 1;
-			dice2 = (int)(Math.random() * 6) + 1;
-			System.out.printf("주사위의 눈은 (%d,%d)입니다.\n",dice1, dice2);
-			if(dice1 + dice2 == 5) {
-				isTrue = !isTrue;
-			}
-		}
-		
-		
-		// p161 4번
-		for(int x = 1; x <= 10; x++) {
-			for(int y = 1; y <= 10; y++) {
-				if((4*x)+(5*y)==60) {
-					System.out.println("4x + 5y = 60의 해는 (" + x + "," + y + ")");
-				}
-			}
-		}
+//		int dice1;
+//		int dice2;
+//		boolean isTrue = true;
+//		while(isTrue) {
+//			dice1 = (int)(Math.random() * 6) + 1;
+//			dice2 = (int)(Math.random() * 6) + 1;
+//			System.out.printf("주사위의 눈은 (%d,%d)입니다.\n",dice1, dice2);
+//			if(dice1 + dice2 == 5) {
+//				isTrue = !isTrue;
+//			}
+//		}
+//		
+//		
+//		// p161 4번
+//		for(int x = 1; x <= 10; x++) {
+//			for(int y = 1; y <= 10; y++) {
+//				if((4*x)+(5*y)==60) {
+//					System.out.println("4x + 5y = 60의 해는 (" + x + "," + y + ")");
+//				}
+//			}
+//		}
 		
 	}
 }
--- P20220412/src/co/edu/loop/WhileHomework.java
+++ P20220412/src/co/edu/loop/WhileHomework.java
@@ -1,5 +1,7 @@
 package co.edu.loop;
 
+import java.util.Scanner;
+
 public class WhileHomework {
 
 	public static void main(String[] args) {
@@ -10,7 +12,59 @@ public class WhileHomework {
 //		      이기는 경우 1=2  2=3  3=1 
 //		      이외의 경우는 짐.
 //		      가위 (1) -> You Win. You lose.
-
+		/*1번
+		
+		Scanner scn = new Scanner(System.in);
+		int dice;
+		int answer;
+		boolean isTrue = true;
+		dice = (int)(Math.random()*6) + 1;
+		while(isTrue) {
+			System.out.print("주사위 숫자를 입력하세요:");
+			answer = scn.nextInt();
+			if(answer == dice) {
+				System.out.printf("정답은 %d입니다.",answer);
+				break;
+			}
+			System.out.println("틀렸습니다. 다시 입력하세요.");
+		}*/
+		
+		Scanner scn = new Scanner(System.in);
+		int you , me;
+		you = (int)(Math.random()*3) + 1;
+		boolean isTrue = true;
+	
+		
+		while(isTrue) {
+			System.out.print("가위(1) 바위(2) 보(3) 중 하나를 고르십시오.");
+			me = scn.nextInt();
+			
+			if(me == 1) {
+				if(you == 3) {
+					System.out.println("You Win.");
+				} else {
+					System.out.println("You Lose");
+				}
+			}
+			
+			if(me == 2) {
+				if(you == 1) {
+					System.out.println("You Win.");
+				} else {
+					System.out.println("You Lose");
+				}
+			}
+			
+			if(me == 3) {
+				if(you == 2) {
+					System.out.println("You Win.");
+				} else {
+					System.out.println("You Lose.");
+				}
+			}
+			
+		}
+		System.out.println("상대는 %d를 냈습니다." + you);
 	}
 
 }
