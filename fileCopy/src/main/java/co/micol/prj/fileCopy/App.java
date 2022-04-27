d5272958af73def188c8f16c172b9c9df338e6d9
admin <admin@YD2-06>
2022-04-27 13:52:53 +0900

    111

---
+++ fileCopy/src/main/java/co/micol/prj/fileCopy/App.java
@@ -0,0 +1,16 @@
+package co.micol.prj.fileCopy;
+
+import co.micol.prj.CopyExam.CopyExam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	CopyExam copy = new CopyExam();
+    	copy.copy();
+    }
+}
